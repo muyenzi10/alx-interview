dbf2c7afd78d2dd4aad8c4480422e3c91caa4834
muyenzi10 <muyenzija10@gmail.com>
2024-06-12 06:31:20 -0700

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Minimum op
+""" Minimum Operations
     """
 
 
 def minOperations(n: int) -> int:
-    """ Minims """
+    """ Minimum Operations needed to get n H characters """
     next = 'H'
     body = 'H'
     op = 0
